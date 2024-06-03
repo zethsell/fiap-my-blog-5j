2aff471bc8a6914dbcbdd49bfac12aaa384b3421
Jaison <jaisonschmidt@gmail.com>
2024-06-03 20:00:07 -0300

    feat: Configura React Router Dom

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Home from "./pages/Home";
-import Post from "./pages/Post";
-
-function App() {
-  return <Post />;
-}
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Post from './pages/Post';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Home />,
+  },
+  {
+    path: "post/:slug",
+    element: <Post />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
