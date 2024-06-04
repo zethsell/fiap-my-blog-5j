32cfa9393a6cf167717f171f61ccba511a153460
Jaison <jaisonschmidt@gmail.com>
2024-06-03 21:45:49 -0300

    feat: Finaliza home buscando dados do contentful

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,51 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import Card from "../components/Card";
 import Layout from "../components/Layout";
 
+import { contentfulClient } from "../utils/createContentfulClient";
+
 function Home() {
+    const [categories, setCategories] = useState([]);
+    const [posts, setPosts] = useState([]);
+
+    const getCategories = async () => {
+        try {
+            const response = await contentfulClient.getEntries({
+                content_type: 'blogCategory5j',
+            });
+    
+            setCategories(response.items);
+        } catch (error) {
+            console.log('Erro ao obter categorias', error);
+            setCategories([]);
+        }
+    };
+
+    const getPosts = async () => {
+        try {
+            // se a promise for resolvida, o resultado é capturado aqui
+            const response = await contentfulClient.getEntries({
+                content_type: 'blogPost5j',
+                limit: 5,
+                order: '-sys.createdAt',
+            });
+
+            console.log(response.items);
+            setPosts(response.items);
+        } catch (error) {
+            // se a promise for rejeitada, o erro é capturado aqui
+            console.log('Erro ao obter posts', error);
+            setPosts([]);
+        }
+    };
+
+    useEffect(() => {
+        getCategories();
+        getPosts();
+    }, []); // useEffect -> onLoad do componente Home
+
     return (
         <Layout>
             <div className="container my-4">
@@ -13,10 +55,14 @@ function Home() {
                             Últimos posts
                         </h2>
 
-                        <Card
-                            title={"Post 1"}
-                            text={"Texto do post 1"}
-                        />
+                        {posts.map( (item) => (
+                            <Card
+                                key={item.sys.id}
+                                title={item.fields.blogPostTitle}
+                                text={item.fields.blogPostDescription}
+                                link={'/post/' + item.fields.blogPostSlug}
+                            />
+                        ) )}
 
                         <Link to="/post" className="btn btn-dark mt-4">
                             Ver todos os posts
@@ -27,8 +73,7 @@ function Home() {
                         <h2>Categorias</h2>
 
                         <ul>
-                            <li>Geral</li>
-                            <li>Frameworks</li>
+                            {categories.map( (item) => <li key={item.sys.id}>{item.fields.blogCategoryTitle}</li> )}
                         </ul>
                     </aside>
                 </div>                
